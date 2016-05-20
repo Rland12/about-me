dea7bbede11438046156337057062f3944795b7a
Rland12 <rland7596@withs.org>
2016-05-20 13:38:36 +0000

    About Me

---
+++ Site.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#message').focus();
+
+	$("#post-btn").click(function(){
+
+		var text = $("#message").val();
+		
+		 $("#wall").prepend("<div>" +"<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text );
+	 
+		  $("img").width(90);
+		   $("img").css("margin-right", 20);
+		 $("img").css("margin-top", 43);
+	});
+});
