b0ea211ef340a9a72a579696fa019313c8440466
Rland12 <rland7596@withs.org>
2017-03-27 20:34:57 +0000

    view messages when typed and New Font

--- site.js
+++ site.js
@@ -1,14 +1,24 @@
-$(document).ready(function(){
-	$('#message').focus();
+$(document).ready(function() {
 
-	$("#post-btn").click(function(){
+	$("#enter").click(function(event) {
+		event.preventDefault();
 
-		var text = $("#message").val();
+		var text = $("#text").val();
+
+		$('#text').focus();
+
+		$("#text").val("");
+
+
+		$("#wall").prepend("<div>" + "<img id='profile' src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text + "</div>");
+		$("#wall").css("background-color", "rgba(0, 0, 0, 0.8");
+		$("#wall").css("font-size", 15);
 		
-		 $("#wall").prepend("<div>" +"<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text );
-	 
-		  $("img").width(90);
-		   $("img").css("margin-right", 20);
-		 $("img").css("margin-top", 43);
+		$("#profile").width(90);
+		$("#profile").css("margin-right", 20);
+		$("#profile").css("margin-top", 43);
+
+
 	});
+
 });
